da2c48f0a8e77ad2605e70a68241bca3f261304f
Brian W. Wolter <brian@brianwolter.com>
2020-02-04 13:29:30 -0500

    Adding convenience time func

---
+++ v1/time.go
@@ -0,0 +1,11 @@
+package dbx
+
+import (
+	"time"
+)
+
+// Obtain the current timestamp truncated to milliseconds, which
+// is the most precision most databases (including Postgres) support.
+func Now() time.Time {
+	return time.Now().Truncate(time.Millisecond)
+}
