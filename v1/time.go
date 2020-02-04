1bfe39d604c03a4685f8d029e5d1fc51261e42d2
Brian W. Wolter <brian@brianwolter.com>
2020-02-04 14:39:30 -0500

    Now() is UTC

--- v1/time.go
+++ v1/time.go
@@ -7,5 +7,5 @@ import (
 // Obtain the current timestamp truncated to milliseconds, which
 // is the most precision most databases (including Postgres) support.
 func Now() time.Time {
-	return time.Now().Truncate(time.Millisecond)
+	return time.Now().UTC().Truncate(time.Millisecond)
 }
