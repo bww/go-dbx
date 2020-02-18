3d15530b41ada386e6af5b1fe2ef9527fb4f3997
Brian W. Wolter <brian@brianwolter.com>
2020-02-18 11:24:07 -0500

    Adding tests

--- v1/internal/fixtures/migrations/001_up_base.sql
+++ v1/internal/fixtures/migrations/001_up_base.sql
@@ -16,3 +16,13 @@ create table test_entity_r_another_entity (
   x     varchar(64)     not null references another_entity,
   primary key(a, x)
 );
+
+create table omitempty_entity (
+  z     varchar(64)     primary key not null,
+  a     varchar(64),
+  b     integer,
+  c     bigint,
+  d     boolean,
+  e     double precision,
+  f     bytea
+);
--- v1/persist/persist_test.go
+++ v1/persist/persist_test.go
@@ -21,9 +21,10 @@ func TestMain(m *testing.M) {
 }
 
 const (
-	testDB       = "dbx_v1_persist_test"
-	testTable    = "test_entity"
-	anotherTable = "another_entity"
+	testDB         = "dbx_v1_persist_test"
+	testTable      = "test_entity"
+	anotherTable   = "another_entity"
+	omitemptyTable = "omitempty_entity"
 )
 
 type DontUseThisExportedEntity struct {
@@ -212,6 +213,63 @@ func TestPersist(t *testing.T) {
 	count, err = pst.Count(`SELECT COUNT(*) FROM ` + testTable)
 	if assert.Nil(t, err, fmt.Sprint(err)) {
 		assert.Equal(t, 0, count)
+	}
+
+}
+
+type emptyEntity struct {
+	Z string  `db:"z,pk"`
+	A string  `db:"a,omitempty"`
+	B int     `db:"b,omitempty"`
+	C uint64  `db:"c,omitempty"`
+	D bool    `db:"d,omitempty"`
+	E float64 `db:"e,omitempty"`
+	F []byte  `db:"f,omitempty"`
+}
+
+func TestPersistOmitEmpty(t *testing.T) {
+	pst := New(test.DB(), entity.NewFieldMapper(), registry.New(), ident.AlphaNumeric(32))
+	var err error
+
+	e1 := &emptyEntity{}
+	err = pst.Store(omitemptyTable, e1, nil)
+	if assert.Nil(t, err, fmt.Sprint(err)) {
+		assert.Len(t, e1.Z, 32)
+	}
+
+	var c1 emptyEntity
+	err = pst.Fetch(omitemptyTable, &c1, e1.Z)
+	if assert.Nil(t, err, fmt.Sprint(err)) {
+		assert.Equal(t, "", c1.A)
+		assert.Equal(t, int(0), c1.B)
+		assert.Equal(t, uint64(0), c1.C)
+		assert.Equal(t, false, c1.D)
+		assert.Equal(t, float64(0), c1.E)
+		assert.Equal(t, []byte(nil), c1.F)
+	}
+
+	e2 := &emptyEntity{
+		A: "String here.",
+		B: 999,
+		C: 888,
+		D: true,
+		E: 77.77,
+		F: []byte("And here"),
+	}
+	err = pst.Store(omitemptyTable, e2, nil)
+	if assert.Nil(t, err, fmt.Sprint(err)) {
+		assert.Len(t, e2.Z, 32)
+	}
+
+	var c2 emptyEntity
+	err = pst.Fetch(omitemptyTable, &c2, e2.Z)
+	if assert.Nil(t, err, fmt.Sprint(err)) {
+		assert.Equal(t, "String here.", c2.A)
+		assert.Equal(t, int(999), c2.B)
+		assert.Equal(t, uint64(888), c2.C)
+		assert.Equal(t, true, c2.D)
+		assert.Equal(t, float64(77.77), c2.E)
+		assert.Equal(t, []byte("And here"), c2.F)
 	}
 
 }
