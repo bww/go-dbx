5b9fd217dab55a6b8f8851360e0927d85a5503b4
Brian W. Wolter <brian@brianwolter.com>
2020-02-28 19:03:34 -0500

    Tests

--- v1/filter/filter_test.go
+++ v1/filter/filter_test.go
@@ -2,11 +2,13 @@ package filter
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFilterOptions(t *testing.T) {
+	now, later := time.Now(), time.Now().Add(time.Hour)
 	tests := []struct {
 		Options []Option
 		Expect  Filter
@@ -21,6 +23,34 @@ func TestFilterOptions(t *testing.T) {
 			},
 			Filter{
 				Limit: Range{2, 20},
+			},
+		},
+		{
+			[]Option{
+				WithLimit(Range{2, 20}),
+				WithOrder(Ascending),
+			},
+			Filter{
+				Limit: Range{2, 20},
+				Order: Ascending,
+			},
+		},
+		{
+			[]Option{
+				WithTimeframe(NewTimeframe(now, later)),
+			},
+			Filter{
+				Timeframe: NewTimeframe(now, later),
+			},
+		},
+		{
+			[]Option{
+				WithTimeframe(NewTimeframe(now, later)),
+				WithOrder(Descending),
+			},
+			Filter{
+				Timeframe: NewTimeframe(now, later),
+				Order:     Descending,
 			},
 		},
 	}
