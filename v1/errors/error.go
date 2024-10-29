90cbcb8539a49a01a38346490fa0442fb7d42f6f
Brian W. Wolter <brianwolter@gmail.com>
2024-10-29 08:57:34 -0700

    ...

--- v1/errors/error.go
+++ v1/errors/error.go
@@ -1,7 +1,6 @@
 package errors
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/bww/go-util/v1/text"
@@ -19,17 +18,6 @@ func New(err error, sql string) *Error {
 	}
 }
 
-// Wrap produces a new [github.com/bww/go-dbx/errors.Error] only if the provided
-// error is not already one.
-func Wrap(err error, sql string) error {
-	var dbxerr *Error
-	if errors.As(err, &dbxerr) {
-		return err
-	} else {
-		return New(err, sql)
-	}
-}
-
 func (e *Error) Error() string {
 	return fmt.Sprintf("%v <%s>", e.cause, text.CollapseSpaces(e.stmt))
 }
@@ -38,8 +26,7 @@ func (e *Error) Unwrap() error {
 	return e.cause
 }
 
-// Statement produces the SQL statement that produced the error, if this is
-// known.
+// Statement produces the SQL statement that produced the error, if this is known.
 func (e *Error) Statement() string {
 	return e.stmt
 }
